a35ec11783e70a650afe589e01a4a640001968a1
enckhausen <72ceiba@gmail.com>
2026-07-13 16:30:23 -0600

    Updated from Publii

--- functions/es/[[path]].js
+++
@@ -1,8 +0,0 @@
-export async function onRequest(context) {
-  const url = new URL(context.request.url);
-  
-  // Directly points to your separate Spanish Pages project URL
-  const upstreamUrl = `https://pages.dev${url.pathname}${url.search}`;
-  
-  return fetch(upstreamUrl, context.request);
-}
