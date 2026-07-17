af3f3b3efca9d32a08357aad3dd5e370c66db673
enckhausen <williamenck@ik.me>
2026-07-17 15:13:08 -0600

    xxxx

---
+++ wp-content/themes/yaatra-blog/inc/customizer/custom-controls/radio-image/radio-image.js
@@ -0,0 +1,22 @@
+/**
+ * Custom scripts for Radio Image Control
+ *
+ * @package Yaatra Blog
+ */
+
+wp.customize.controlConstructor['cv-radio-image'] = wp.customize.Control.extend({
+
+	ready: function() {
+
+		'use strict';
+
+		var control = this;
+
+		// Change the value
+		this.container.on( 'click', 'input', function() {
+			control.setting.set( jQuery( this ).val() );
+		});
+
+	}
+	
+});
